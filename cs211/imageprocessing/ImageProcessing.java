062d163d914feb012ee58c29e6c006acd8571210
Loïc Charrière <loic.charriere@bluewin.ch>
2015-06-13 09:49:28 +0200

    All about image processing
    imageProcessing
    milestone3
    cameraLineDetection

--- cs211/imageprocessing/ImageProcessing.java
+++ cs211/imageprocessing/ImageProcessing.java
@@ -1,282 +1,78 @@
-package imageprocessing;
-
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Collections;
+package imageProcessing;
 
 import processing.core.PApplet;
 import processing.core.PImage;
-import processing.core.PVector;
 
 @SuppressWarnings("serial")
 public class ImageProcessing extends PApplet {
-	final float HUE_FLOOR = 100.f;
-	final float HUE_CEIL = 140.f;
-	final float BRIGHTNESS_THRESHOLD = 25.f;
-	final float SATURATION_THRESHOLD = 80.f;
-	final float SOBEL_THRESHOLD = 0.3f;
-
-	private int height, width;
-
 	PImage img;
-	int[] accumulator;
-
-	List<PVector> bestCandidatesVect = new ArrayList<PVector>();
+	HScrollbar bar1 ,bar2, bar3, bar4;
+	int shiftMode = 0;
+	final int modeNb = 6;
 
 	public void setup() {
+		size(800, 600);
 		img = loadImage("board1.jpg");
-		img.resize((img.width / 2), img.height / 2);
-		width = img.width;
-		height = img.height;
-
-		size(2 * width + height, height);
+		bar1 = new HScrollbar(this, 0, 590, 800, 10);
+		bar2 = new HScrollbar(this, 0, 575, 800, 10);
+		bar3 = new HScrollbar(this, 0, 560, 800, 10);
+		bar4 = new HScrollbar(this, 0, 545, 800, 10);
 	}
 
 	public void draw() {
 		background(color(0, 0, 0));
-		noLoop();
-		PImage edgeImg = img;
-		edgeImg = colorThresholding(edgeImg, HUE_FLOOR, HUE_CEIL);
-		edgeImg = saturationThresholding(edgeImg, SATURATION_THRESHOLD);
-		edgeImg = gaussianBlur(edgeImg);
-		edgeImg = thresholding(edgeImg, BRIGHTNESS_THRESHOLD);
-		edgeImg = sobel(edgeImg, SOBEL_THRESHOLD);
-
-		PImage accImg = hough(edgeImg);
-		accImg.resize(height, height);
-
-		image(img, 0, 0);
-		int minVotes = 100;
-		int nLines = 4;
-
-		detectBoard(edgeImg, nLines, minVotes);
-		image(accImg, width, 0);
-		image(edgeImg, height + width, 0);
-
+		switch (shiftMode) {
+		// part I step 1-2
+		case 0:
+			noLoop();
+			image(thresholding(img, 128), 0, 0);
+			break;
+		// part I step 3
+		case 1:
+			image(thresholding(img, bar3.getPos() * 255), 0, 0);
+			bar3.display();
+			bar3.update();
+			break;
+		// part I step 4
+		case 2:
+			image(colorThresholding(img, bar1.getPos() * 255, bar2.getPos() * 255), 0, 0);
+			println("ceil: "+ bar1.getPos() * 255 + " floor: " + bar2.getPos() * 255);
+			bar1.display();
+			bar2.display();
+			bar1.update();
+			bar2.update();
+			break;
+		// step II step 1-2
+		case 3:
+			noLoop();
+			image(gaussianBlur(img), 0, 0);
+			break;
+		// part II step 3
+		case 4:
+			noLoop();
+			image(sobel(img, 0.3f), 0, 0);
+			break;
+		// part III
+		case 5:
+			image(boardDetection(img, bar1.getPos() * 255, bar2.getPos() * 255, bar3.getPos() * 255, bar4.getPos()), 0, 0);
+			bar1.display();
+			bar2.display();
+			bar3.display();
+			bar4.display();
+			bar1.update();
+			bar2.update();
+			bar3.update();
+			bar4.update();
+			break;
+		default:
+		}
 	}
-
-	public PImage hough(PImage edgeImg) {
-		float discretizationStepsPhi = 0.06f;
-		float discretizationStepsR = 2.5f;
-		// dimensions of the accumulator
-		int phiDim = (int) (Math.PI / discretizationStepsPhi);
-		int rDim = (int) (((edgeImg.width + edgeImg.height) * 2 + 1) / discretizationStepsR);
-
-		// pre-compute the sin and cos values
-		float[] tabSin = new float[phiDim];
-		float[] tabCos = new float[phiDim];
-		float ang = 0;
-		float inverseR = 1.f / discretizationStepsR;
-		for (int accPhi = 0; accPhi < phiDim; ang += discretizationStepsPhi, accPhi++) {
-			// we can also pre-multiply by (1/discretizationStepsR) since we
-			// need it in the Hough loop
-			tabSin[accPhi] = (float) (Math.sin(ang) * inverseR);
-			tabCos[accPhi] = (float) (Math.cos(ang) * inverseR);
-		}
-
-		// println("imgWidth:" + edgeImg.width + "\nimgheight:" + edgeImg.height
-		// + "\nphiDim:" + phiDim + "\nrDim:" + rDim);
-		// our accumulator (with a 1 pix margin around)
-		accumulator = new int[(phiDim + 2) * (rDim + 2)];
-		// Fill the accumulator: on edge points (ie, white pixels of the edge
-		// image), store all possible (r, phi) pairs describing lines going
-		// through the point.
-		for (int y = 0; y < edgeImg.height; y++) {
-			for (int x = 0; x < edgeImg.width; x++) {
-				// Are we on an edge?
-				if (brightness(edgeImg.pixels[y * edgeImg.width + x]) != 0) {
-					// ...determine here all the lines (r, phi) passing through
-					// pixel (x,y), convert (r,phi) to coordinates in the
-					// accumulator, and increment accordingly the accumulator.
-					for (int coordPhi = 1; coordPhi < phiDim + 1; coordPhi++) {
-						float phi = (coordPhi - 1) * discretizationStepsPhi;
-						float r = x * cos(phi) + y * sin(phi);
-						int coordR = Math.round((r / discretizationStepsR))
-								+ (rDim - 1) / 2;
-						accumulator[coordPhi * (rDim + 2) + 1 + coordR] += 1;
-					}
-				}
-			}
-		}
-
-		PImage accImg = createImage(rDim + 2, phiDim + 2, ALPHA);
-		for (int i = 0; i < accumulator.length; i++) {
-			accImg.pixels[i] = color(min(255, accumulator[i]));
-		}
-		accImg.updatePixels();
-		return accImg;
-
-	}
-
-	public void detectBoard(PImage edgeImg, int nLines, int minVotes) {
-		ArrayList<Integer> bestCandidates = new ArrayList<Integer>();
-
-		float discretizationStepsPhi = 0.06f;
-		float discretizationStepsR = 2.5f;
-		// dimensions of the accumulator
-		int phiDim = (int) (Math.PI / discretizationStepsPhi);
-		int rDim = (int) (((edgeImg.width + edgeImg.height) * 2 + 1) / discretizationStepsR);
-
-		// pre-compute the sin and cos values
-		float[] tabSin = new float[phiDim];
-		float[] tabCos = new float[phiDim];
-		float ang = 0;
-		float inverseR = 1.f / discretizationStepsR;
-		for (int accPhi = 0; accPhi < phiDim; ang += discretizationStepsPhi, accPhi++) {
-			// we can also pre-multiply by (1/discretizationStepsR) since we
-			// need it in the Hough loop
-			tabSin[accPhi] = (float) (Math.sin(ang) * inverseR);
-			tabCos[accPhi] = (float) (Math.cos(ang) * inverseR);
-		}
-
-		// size of the region we search for a local maximum
-		int neighbourhood = 10;
-		// only search around lines with more that this amount of votes
-		// (to be adapted to your image)
-		for (int accR = 0; accR < rDim; accR++) {
-			for (int accPhi = 0; accPhi < phiDim; accPhi++) {
-				// compute current index in the accumulator
-				int idx = (accPhi + 1) * (rDim + 2) + accR + 1;
-				if (accumulator[idx] > minVotes) {
-					boolean bestCandidate = true;
-					// iterate over the neighbourhood
-					for (int dPhi = -neighbourhood / 2; dPhi < neighbourhood / 2 + 1; dPhi++) {
-						// check we are not outside the image
-						if (accPhi + dPhi < 0 || accPhi + dPhi >= phiDim)
-							continue;
-						for (int dR = -neighbourhood / 2; dR < neighbourhood / 2 + 1; dR++) {
-							// check we are not outside the image
-							if (accR + dR < 0 || accR + dR >= rDim)
-								continue;
-							int neighbourIdx = (accPhi + dPhi + 1) * (rDim + 2)
-									+ accR + dR + 1;
-							if (accumulator[idx] < accumulator[neighbourIdx]) {
-								// the current idx is not a local maximum!
-								bestCandidate = false;
-								break;
-							}
-						}
-						if (!bestCandidate)
-							break;
-					}
-					if (bestCandidate) {
-						// the current idx *is* a local maximum
-						bestCandidates.add(idx);
-					}
-				}
-			}
-		}
-		
-		Collections.sort(bestCandidates, new HoughComparator(accumulator));
-
-		for (int i = 0; i < min(nLines, bestCandidates.size()); i++) {
-			int idx = bestCandidates.get(i);
-			// first, compute back the (r, phi) polar coordinates:
-			int accPhi = Math.round(idx / (rDim + 2)) - 1;
-			int accR = idx - (accPhi + 1) * (rDim + 2) - 1;
-			float r = (accR - (rDim - 1) * 0.5f) * discretizationStepsR;
-			float phi = accPhi * discretizationStepsPhi;
-
-			// Cartesian equation of a line: y = ax + b
-			// in polar, y = (-cos(phi)/sin(phi))x + (r/sin(phi))
-			// => y = 0 : x = r / cos(phi)
-			// => x = 0 : y = r / sin(phi)
-
-			// compute the intersection of this line with the 4 borders
-			// of
-			// the image
-			int x0 = 0;
-			int y0 = Math.round(r / sin(phi));
-			int x1 = Math.round(r / cos(phi));
-			int y1 = 0;
-			int x2 = edgeImg.width;
-			int y2 = Math.round(-cos(phi) / sin(phi) * x2 + r / sin(phi));
-			int y3 = edgeImg.width;
-			int x3 = Math.round(-(y3 - r / sin(phi)) * (sin(phi) / cos(phi)));
-
-			// Finally, plot the lines
-			stroke(204, 102, 0);
-			if (y0 > 0) {
-				if (x1 > 0)
-					line(x0, y0, x1, y1);
-				else if (y2 > 0)
-					line(x0, y0, x2, y2);
-				else
-					line(x0, y0, x3, y3);
-			} else {
-				if (x1 > 0) {
-					if (y2 > 0)
-						line(x1, y1, x2, y2);
-					else
-						line(x1, y1, x3, y3);
-				} else
-					line(x2, y2, x3, y3);
-			}
-		}
-
-		for (int i = 0; i < min(nLines, bestCandidates.size()); i++) {
-			int idx = bestCandidates.get(i);
-			int accPhi = Math.round(idx / (rDim + 2)) - 1;
-			int accR = idx - (accPhi + 1) * (rDim + 2) - 1;
-			float r = (accR - (rDim - 1) * 0.5f) * discretizationStepsR;
-			float phi = accPhi * discretizationStepsPhi;
-			bestCandidatesVect.add(new PVector(r, phi));
-		}
-		
-		QuadGraph quadGraph = new QuadGraph();
-		quadGraph.build(bestCandidatesVect, width, height);
-		List<int[]> cycles = quadGraph.cycles;
-		
-		List<PVector> toDraw = new ArrayList<>();
-		
-		if (cycles.isEmpty()){
-			toDraw = bestCandidatesVect.subList(0, 4);
-		} else {
-			for (int i = 0; i < cycles.get(0).length; i++){
-				toDraw.add(bestCandidatesVect.get(cycles.get(0)[i]));
-			}
-		}
-		getIntersections(toDraw);
-	}
-
-	public ArrayList<PVector> getIntersections(List<PVector> lines) {
-		ArrayList<PVector> intersections = new ArrayList<PVector>();
-		for (int i = 0; i < lines.size() - 1; i++) {
-			PVector line1 = lines.get(i);
-			for (int j = i + 1; j < lines.size(); j++) {
-				PVector line2 = lines.get(j);
-				// compute the intersection and add it to 'intersections'
-				float d = cos(line2.y) * sin(line1.y) - cos(line1.y)
-						* sin(line2.y);
-				float x = (line2.x * sin(line1.y) - line1.x * sin(line2.y)) / d;
-				float y = (-line2.x * cos(line1.y) + line1.x * cos(line2.y))
-						/ d;
-				intersections.add(new PVector(x, y));
-				// draw the intersection
-				fill(255, 128, 0);
-				ellipse(x, y, 10, 10);
-			}
-		}
-		return intersections;
-	}
-
+	
 	public PImage thresholding(PImage img, float threshold) {
 		PImage result = createImage(width, height, ALPHA);
 		for (int i = 0; i < img.width * img.height; i++) {
 			if (brightness(img.pixels[i]) > threshold) {
 				result.pixels[i] = color(255);
-			}
-		}
-		return result;
-	}
-
-	public PImage saturationThresholding(PImage img, float threshold) {
-		PImage result = createImage(width, height, ALPHA);
-		for (int i = 0; i < img.width * img.height; i++) {
-			if (saturation(img.pixels[i]) > threshold) {
-				result.pixels[i] = img.pixels[i];
-			} else {
-				result.pixels[i] = color(0);
 			}
 		}
 		return result;
@@ -337,6 +133,15 @@ public class ImageProcessing extends PApplet {
 		}
 		return result;
 	}
+	
+	PImage boardDetection(PImage img, float hueFloor, float hueCeil, float brThreshold, float sobelThreshold) {
+		PImage result = img;
+		result = colorThresholding(result, hueFloor, hueCeil);
+		//result = gaussianBlur(result);
+		result = thresholding(result, brThreshold);
+		result = sobel(result, sobelThreshold);
+		return result;
+	}
 
 	// HELPERS METHODS
 	public float[] convolute(PImage img, float[][] kernel, float weight) {
@@ -357,6 +162,21 @@ public class ImageProcessing extends PApplet {
 			result[px] = sum / weight;
 		}
 		return result;
+	}
+
+	public void keyPressed() {
+		if (key == CODED) {
+			switch (keyCode) {
+			case SHIFT:
+				shiftMode += 1;
+				if (shiftMode >= modeNb) {
+					shiftMode = 0;
+				}
+				loop();
+			default:
+				break;
+			}
+		}
 	}
 
 }
--- cs211/milestone3/QuadGraph.java
+++ cs211/milestone3/QuadGraph.java
@@ -1,4 +1,4 @@
-package imageprocessing;
+package milestone3;
 
 import java.util.List;
 import java.util.ArrayList;
